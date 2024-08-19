02851337ea0e90f43a121a1bd565f67ceb72f1ed
youngsoosoo <lovepys01@naver.com>
2024-08-19 21:04:53 +0900

    feat: board created

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BoardService } from './board.service';
+import { Board } from './board.model';
 
-@Controller('board')
-export class BoardController {}
+@Controller('boards')
+export class BoardController {
+  constructor(private boardsService: BoardService) {}
+
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
+}
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
+  }
+}
