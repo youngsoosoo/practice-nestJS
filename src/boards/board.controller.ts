3b7917a92030e4c3c5f8b7f0ee32e79766f605d3
youngsoosoo <lovepys01@naver.com>
2024-08-26 19:57:56 +0900

    feat: 커스텀 파이프 생성, update 유효성 체크

--- src/boards/board.controller.ts
+++ src/boards/board.controller.ts
@@ -12,6 +12,7 @@ import {
 import { BoardService } from './board.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardController {
@@ -41,7 +42,7 @@ export class BoardController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ) {
     return this.boardsService.updateBoardStatus(id, status);
   }
